aa4b329b7b48503409bf3ed6de6e7a0c76139f5c
Sheila-a <ugwuodo20@gmail.com>
2022-03-15 22:34:16 +0100

    Task 1: Printing Alphabet using putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,7 +8,7 @@
  * Return: Always 0.
  */
 
-int print_alphabet(void)
+void print_alphabet(void)
 {
 char letter; /*Decarling statement*/
 
@@ -20,5 +20,4 @@ _putchar(letter);/*print letter*/
 
 _putchar('\n'); /*new line*/
 
-return (0);
 }
